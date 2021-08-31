f6db7d62de978e8d3e66c3017c3bc55847339add
James Giusti <jamez318@gmail.com>
2021-08-31 15:21:55 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(rideLength) {
+  if(rideLength <= 400){
+    return 'This one is on me!'
+  } else if(rideLength >2000 && rideLength < 2500){
+    return 'I will gladly take your thirty bucks.'
+  } else if(rideLength > 2500){
+    return 'No can do.'
+  }
+  return scuberGreetingForFeet;
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+
+
+
+function ternaryCheckCity(city){
+let response = city === "NYC" ? 'Ok, sounds good.' : 'No go.';
+
+return response;
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+
+
+
+function switchOnCharmFromTip(tipSize){
+ let response;
+  switch(tipSize) {
+    case 'generous':
+      response = 'Thank you so much.';
+      break;
+    case 'not as generous':
+      response = 'Thank you.';
+      break;
+    default:
+      response = 'Bye.';
+      break;
+  }
+  return response;
 }
